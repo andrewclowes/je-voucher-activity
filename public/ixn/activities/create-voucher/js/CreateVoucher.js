2ce05af5504643350a6f94c03334b0d5a412340d
Andrew Clowes <andrew.clowes@Just-eat.com>
2016-02-16 16:41:25 +0000

    Added project files

---
+++ public/ixn/activities/create-voucher/js/CreateVoucher.js
@@ -0,0 +1,78 @@
+define( function( require ) {
+	var Postmonger = require( 'postmonger' );
+	var $ = require( 'vendor/jquery.min' );
+
+    var connection = new Postmonger.Session();
+	var tokens;
+	var endpoints;
+
+    $(window).ready(function() {
+        connection.trigger('ready');
+		connection.trigger('requestTokens');
+		connection.trigger('requestEndpoints');
+    })
+
+	// This listens for Journey Builder to send tokens
+	// Parameter is either the tokens data or an object with an
+	// "error" property containing the error message
+	connection.on('getTokens', function( data ) {
+		if( data.error ) {
+			console.error( data.error );
+		} else {
+			tokens = data;
+		}
+	});
+
+	/**
+		If you want to have a multi-step configuration view, you need to manage the DOM manually.
+		You can filter what changes to make by implementing the following type of logic when Postmonger from the server triggers an "updateStep" call.
+		// connection.on('updateStep', step ) {
+
+			if( step  >= 1 && step <= 3 ) {
+				$('.step').hide(); // All DOM elements which are steps should have this class (this hides them all)
+				$('#step' + step ).show(); // This selectively only displays the current step
+				// Allow the user to make any changes and when you're ready, use:
+				connection.trigger( 'updateStep', step );
+			}
+		}
+	**/
+
+	// This listens for Journey Builder to send endpoints
+	// Parameter is either the endpoints data or an object with an
+	// "error" property containing the error message
+	connection.on('getEndpoints', function( data ) {
+		if( data.error ) {
+			console.error( data.error );
+		} else {
+			endpoints = data;
+		}
+	});
+
+    connection.on('requestPayload', function() {
+	 var payload = {};
+
+        payload.options = {
+
+        };
+
+		//TODO: Shouldn't this come from the data?
+        payload.flowDisplayName = 'Create Voucher';
+
+        connection.trigger('getPayload', payload);
+    });
+
+	// Journey Builder broadcasts this event to us after this module
+	// sends the "ready" method. JB parses the serialized object which
+	// consists of the Event Data and passes it to the
+	// "config.js.save.uri" as a POST
+    connection.on('populateFields', function(payload) {
+    });
+
+	// Trigger this method when updating a step. This allows JB to
+	// update the wizard.
+    //connection.trigger('updateStep', nextStep);
+
+	// When everything has been configured for this activity, trigger
+	// the save:
+	// connection.trigger('save',
+});
---
+++ public/ixn/activities/create-voucher/js/main.js
@@ -0,0 +1,27 @@
+requirejs.config({
+    paths: {
+        vendor: '../vendor',
+		postmonger: 'vendor/postmonger'
+    },
+    shim: {
+        'vendor/jquery.min': {
+            exports: '$'
+        },
+		'CreateVoucher': {
+			deps: ['vendor/jquery.min', 'vendor/postmonger']
+		}
+    }
+});
+
+requirejs( ['vendor/jquery.min', 'CreateVoucher'], function( $, CreateVoucher ) {
+	//console.log( 'REQUIRE LOADED' );
+});
+
+requirejs.onError = function( err ) {
+	//console.log( "REQUIRE ERROR: ", err );
+	if( err.requireType === 'timeout' ) {
+		console.log( 'modules: ' + err.requireModules );
+	}
+
+	throw err;
+};

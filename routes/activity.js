67cd7ceebdd5b0253d51e4c658b7b081361f793f
Andrew Clowes <andrew.clowes@Just-eat.com>
2016-02-29 17:50:09 +0000

    Added voucherApi client

--- routes/activity.js
+++ routes/activity.js
@@ -3,79 +3,96 @@ var https         = require('https');
 var activityUtils = require('./activityUtils');
 var config        = require('config');
 var SfmcClient    = require('../lib/sfmc/sfmcClient');
+var VoucherClient = require('../lib/voucherApi/voucherApiClient');
 
 var Activity = function () {
   var sfmcClient = new SfmcClient(config);
+  var voucherClient = new VoucherClient(config);
 
   return {
     execute: function( req, res ) {
-        console.log("================");
-        console.log('ROUTE = EXECUTE');
-        console.log("================");
-        activityUtils.logData( req );
+      console.log("================");
+      console.log('ROUTE = EXECUTE');
+      console.log("================");
+      activityUtils.logData( req );
 
-        var aArgs = req.body.inArguments;
-        var oArgs = {};
+      var aArgs = req.body.inArguments;
+      var oArgs = {};
 
-        for (var i=0; i<aArgs.length; i++) {
-        for (var key in aArgs[i]) {
-            oArgs[key] = aArgs[i][key];
-          }
+      for (var i=0; i<aArgs.length; i++) {
+      for (var key in aArgs[i]) {
+          oArgs[key] = aArgs[i][key];
         }
+      }
 
-        // TODO: Implement request to Voucher API
-        var voucherCode = "TEST_CODE_" + oArgs.voucher.amount;
+      var voucherOptions = {
+        Name: "Implicit Voucher Group",
+        Discount: {
+            VoucherType: "Absolute",
+            Amount: oArgs.voucher.amount
+        }
+      };
 
-        var primaryKeyValue = oArgs.emailAddress;
-        var dataExtensionKey = oArgs.dataExtensionKey;
-        var primaryKeyField = oArgs.dataExtensionPrimaryKey;
-        var voucherCodeField = oArgs.dataExtensionVoucherField;
+      var vouchers = voucherClient.vouchers();
+      vouchers.postVoucher(config.tenant, voucherOptions, function(error, response, body) {
+        if (error) {
+          res.send( 500, error );
+        } else if (response.statusCode != 200) {
+          res.send( 500, body );
+        } else {
+          var bodyObj = JSON.parse(body);
+          if (bodyObj == null || bodyObj.Code == null) {
+            res.send( 500, body );
+          }
 
-        var keys = {}, values = {};
-        keys[primaryKeyField] = primaryKeyValue;
-        values[voucherCodeField] = voucherCode;
+          var primaryKeyValue = oArgs.emailAddress;
+          var dataExtensionKey = oArgs.dataExtensionKey;
+          var primaryKeyField = oArgs.dataExtensionPrimaryKey;
+          var voucherCodeField = oArgs.dataExtensionVoucherField;
 
-        var options = {
-          dataExtensionKey: dataExtensionKey,
-          keys: keys,
-          values: values
-        };
+          var keys = {}, values = {};
+          keys[primaryKeyField] = primaryKeyValue;
+          values[voucherCodeField] = bodyObj.Code;
 
-        var row = sfmcClient.dataExtensionRow(options);
-        row.post(function (error, request, body) {
-           if (error) {
-             console.log("ERROR 500");
-             console.log(error);
-             res.send( 500, error );
-           } else if (body.errorcode) {
-             console.log("ERROR " + body.errorcode);
-             console.log(body);
-             res.send( 500, body );
-           } else {
-             res.send( 200, body );
-           }
-        });
+          var sfmcOptions = {
+            dataExtensionKey: dataExtensionKey,
+            keys: keys,
+            values: values
+          };
+
+          var row = sfmcClient.dataExtensionRow(sfmcOptions);
+          row.post(function (error, request, body) {
+             if (error) {
+               res.send( 500, error );
+             } else if (body.errorcode) {
+               res.send( 500, body );
+             } else {
+               res.send( 200, body );
+             }
+          });
+        }
+      });
     },
     save: function( req, res ) {
-        console.log("================");
-        console.log('ROUTE = SAVE');
-        console.log("================");
-        activityUtils.logData( req );
-        res.send( 200, 'Save' );
+      console.log("================");
+      console.log('ROUTE = SAVE');
+      console.log("================");
+      activityUtils.logData( req );
+      res.send( 200, 'Save' );
     },
     publish: function( req, res ) {
-        console.log("================");
-        console.log('ROUTE = PUBLISH');
-        console.log("================");
-        activityUtils.logData( req );       
-        res.send( 200 );
+      console.log("================");
+      console.log('ROUTE = PUBLISH');
+      console.log("================");
+      activityUtils.logData( req );
+      res.send( 200 );
     },
     validate: function( req, res ) {
-        console.log("================");
-        console.log('ROUTE = VALIDATE');
-        console.log("================");
-        activityUtils.logData( req );
-        res.send( 200 );
+      console.log("================");
+      console.log('ROUTE = VALIDATE');
+      console.log("================");
+      activityUtils.logData( req );
+      res.send( 200 );
     }
   };
 };
